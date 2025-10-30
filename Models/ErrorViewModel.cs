d316966d64b97ac1dd8214cda426a606caa7c7a2
Eric <spiffydude0@gmail.com>
2025-10-30 07:38:30 -0500

    basic website checkpoint

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace techrestorepro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
