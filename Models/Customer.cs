3e39270ea6cbea79a094b35b05d00e00a928ccce
Eric <spiffydude0@gmail.com>
2025-10-30 07:38:30 -0500

    basic website checkpoint

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace techrestorepro.Models
+{
+	public class Customer
+	{
+		public Customer()
+		{
+		}
+
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public int WorkOrderNumber { get; set; }
+
+    }
+}
+
